c6516e89e64e7cfdabeedf3366ad69663a4baab9
Ranadev25 <ranakhandev2025@gmail.com>
2026-09-15 00:45:27 +0600

    test

--- src/app/(admin-dashbord)/dashboard/project/dashboard/totaltasks/page.tsx
+++ src/app/(admin-dashbord)/dashboard/project/dashboard/totaltasks/page.tsx
@@ -1,5 +1,7 @@
-export default function TotalTasksPage() {
-  return (
-    <div>hello</div>
-  )
-}
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
